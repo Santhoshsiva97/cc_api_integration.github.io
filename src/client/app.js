9ce8a58d02becb0d1f7e668f32a54b3fc4b2475b
Santhosh Sivakumar <santhosh.sivakumar@softsquare.biz>
2022-02-03 23:14:59 +0530

    changes in message

--- src/client/app.js
+++ src/client/app.js
@@ -4,8 +4,8 @@ export default class App extends React.Component {
     render() {        
         return (
             <div>
-                <h1>Hey dude!</h1>
-                <div>Welcome to CC API Integration</div>
+                <h1>Hey dude, Welcome Crypto price Integration project!</h1>
+                <div>Site under construction...</div>
             </div>
         );   
     }
