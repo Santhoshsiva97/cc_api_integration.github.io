e8674c0957312ab07003238b474fff4f52dbbf7e
Santhosh Sivakumar <santhosh.sivakumar@softsquare.biz>
2022-02-03 23:01:53 +0530

    first CLI commit of cc price app

---
+++ src/client/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class App extends React.Component {    
+    render() {        
+        return (
+            <div>
+                <h1>Hey dude!</h1>
+                <div>Welcome to CC API Integration</div>
+            </div>
+        );   
+    }
+}
---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+import WebServer from './web.server'
+
+let webServer = new WebServer();
+
+webServer.start()  
+.then((res) => {    
+    console.log(`Web server started on PORT on ${res}!`)  
+})  
+.catch(err => {    
+    console.error(err)    
+    console.error('Failed to start web server')  
+});
